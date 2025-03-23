14e5bafac08d1198c30ff21ff1e18d45c17ab28e
root <root@DESKTOP-J2DOCLQ>
2025-03-23 01:29:50 -0300

    fix: layer Silver And Gold

--- etl/gold_layer.py
+++ etl/gold_layer.py
@@ -1,32 +1,69 @@
 import pandas as pd # type: ignore
 from time import time
 
+columns_to_fill = [
+        'Total_Energy_Consumption_(TWh)',
+        'Per_Capita_Energy_Use_(kWh)',
+        'Renewable_Energy_Share_(%)',
+        'Fossil_Fuel_Dependency_(%)',
+        'Industrial_Energy_Use_(%)',
+        'Household_Energy_Use_(%)',
+        'Carbon_Emissions_(Million_Tons)',
+        'Energy_Price_Index_(USD-kWh)'
+    ]
+
+
 def trasformacao_insercao_dados_gold(engine):
     t_start = time()
-    df_gold = pd.read_csv('SRC/DATA/Silver.csv')
-    media_renovavel_por_pais = df_gold.groupby(["Country"])["Renewable Energy Share (%)"].mean().reset_index()
-    comparacao_fossil_emissoes = df_gold.groupby("Country")[["Fossil Fuel Dependency (%)", "Carbon Emissions (Million Tons)"]].mean().reset_index()
+    df_gold = pd.read_sql_table('Silver_Data', con=engine)
+    for col in columns_to_fill:
+        media_renovavel_por_pais = df_gold.groupby(["Country"])[col].mean().reset_index()
+        media_renovavel_por_pais.to_csv(f"SRC/DATA/GOLD/media_{col}_por_pais.csv", index=False)
+        media_renovavel_por_pais.to_sql(name=f'media_{col}_por_pais_Gold', con=engine, if_exists='append')
 
-    df_inicio = df_gold.groupby("Country").first().reset_index()
-    df_fim = df_gold.groupby("Country").last().reset_index()
+    for col in columns_to_fill:
+        df_inicio = df_gold.groupby("Country").first().reset_index()
+        df_fim = df_gold.groupby("Country").last().reset_index()
 
-    df_crescimento = df_inicio[["Country", "Per Capita Energy Use (kWh)"]].merge(
-        df_fim[["Country", "Per Capita Energy Use (kWh)"]], on="Country", suffixes=("inicio", "fim")
-    )
+        df_crescimento = df_inicio[["Country", col]].merge(
+            df_fim[["Country", col]], on="Country", suffixes=("-inicio", "-fim")
+        )
 
-    df_crescimento["Crescimento (%)"] = ((df_crescimento["Per Capita Energy Use (kWh)fim"] - df_crescimento["Per Capita Energy Use (kWh)inicio"]) / df_crescimento["Per Capita Energy Use (kWh)inicio"]) * 100
-    df_crescimento = df_crescimento.sort_values("Crescimento (%)", ascending=False)
-    correlacao = df_gold[["Energy Price Index (USD/kWh)", "Fossil Fuel Dependency (%)"]].corr()
+        df_crescimento["Crescimento (%)"] = ((df_crescimento[f"{col}-fim"] - df_crescimento[f"{col}-inicio"]) / df_crescimento[f"{col}-inicio"]) * 100
+        df_crescimento = df_crescimento.sort_values("Crescimento (%)", ascending=False)
+        df_crescimento.to_csv(f"SRC/DATA/GOLD/crescimento_{col}_Gold.csv", index=False)
+        df_crescimento.to_sql(name=f'crescimento_{col}_Gold', con=engine, if_exists='append')
+        
+        df_combined = pd.DataFrame()
+    for col in columns_to_fill:
+        media_renovavel_por_pais = df_gold.groupby("Country")[col].mean().reset_index()
+        media_renovavel_por_pais.columns = media_renovavel_por_pais.columns.str.strip()
+        media_renovavel_por_pais = media_renovavel_por_pais.rename(columns={col: f"media_{col}"})
 
-    correlacao.to_csv("SRC/DATA/GOLD/correlacao_precos_fossil.csv")
-    df_crescimento.to_csv("SRC/DATA/GOLD/crescimento_energia_per_capita.csv", index=False)
-    comparacao_fossil_emissoes.to_csv("SRC/DATA/GOLD/comparacao_fossil_emissoes.csv", index=False)
-    media_renovavel_por_pais.to_csv("SRC/DATA/GOLD/media_renovavel_por_pais.csv", index=False)
+        if df_combined.empty:
+            df_combined = media_renovavel_por_pais
+        else:
+            df_combined = df_combined.merge(media_renovavel_por_pais, on="Country", how="outer")
 
-    correlacao.to_sql(name='Correlacao_Gold', con=engine, if_exists='append')
-    df_crescimento.to_sql(name='crescimento_Gold', con=engine, if_exists='append')
-    comparacao_fossil_emissoes.to_sql(name='comparacao_fossil_emissoes_Gold', con=engine, if_exists='append')
-    media_renovavel_por_pais.to_sql(name='media_renovavel_por_pais_Gold', con=engine, if_exists='append')
+        # Crescimento por país
+        df_inicio = df_gold.groupby("Country").first().reset_index()
+        df_fim = df_gold.groupby("Country").last().reset_index()
+
+        df_crescimento = df_inicio[["Country", col]].merge(
+            df_fim[["Country", col]], on="Country", suffixes=("-inicio", "-fim")
+        )
+        df_crescimento["Crescimento (%)"] = ((df_crescimento[f"{col}-fim"] - df_crescimento[f"{col}-inicio"]) / df_crescimento[f"{col}-inicio"]) * 100
+        df_crescimento = df_crescimento.sort_values("Crescimento (%)", ascending=False)
+
+        if "Crescimento (%)" in df_combined.columns:
+            df_combined.rename(columns={"Crescimento (%)": f"Crescimento (%)_{col}"}, inplace=True)
+
+        df_combined = df_combined.merge(df_crescimento[["Country", "Crescimento (%)"]], on="Country", how="outer")
+    df_combined.to_csv('SRC/DATA/GOLD/tabela_combinada.csv', index=False)
+    df_combined.to_sql(name='tabela_combinada_Gold', con=engine, if_exists='append', index=False)
+    print('Fim dos dados transformados')
+
+  
 
     t_end = time()
     print('operaçao gold levou %.3f segundos' % (t_end - t_start))
--- etl/silver_layer.py
+++ etl/silver_layer.py
@@ -2,42 +2,45 @@ import pandas as pd # type: ignore
 from time import time
 
 def limpesa_insercao_dados_silver(engine):
-    df_clean = pd.read_csv('SRC/DATA/energiaGlob.csv')
+    df_clean = pd.read_sql_table('Bronze_Data', con=engine)
     df_clean = df_clean.drop_duplicates()
-    replace_dict = {'@': 'a', '&': 'e', '$': 's'}
+    
+    df_clean.columns = df_clean.columns.str.replace('/', '-', regex=False)
+    df_clean.columns = df_clean.columns.str.replace(' ', '_')
+
+    replace_dict = {'@': 'a', '&': 'e'}
     columns_to_fill = [
-        "Total Energy Consumption (TWh)",
-        "Per Capita Energy Use (kWh)",
-        "Renewable Energy Share (%)",
-        "Fossil Fuel Dependency (%)",
-        "Industrial Energy Use (%)",
-        "Household Energy Use (%)",
-        "Carbon Emissions (Million Tons)",
-        "Energy Price Index (USD/kWh)"
+        'Total_Energy_Consumption_(TWh)',
+        'Per_Capita_Energy_Use_(kWh)',
+        'Renewable_Energy_Share_(%)',
+        'Fossil_Fuel_Dependency_(%)',
+        'Industrial_Energy_Use_(%)',
+        'Household_Energy_Use_(%)',
+        'Carbon_Emissions_(Million_Tons)',
+        'Energy_Price_Index_(USD-kWh)'
     ]
 
     collumns_to_check = [
-        "Renewable Energy Share (%)",
-        "Household Energy Use (%)"
+        'Renewable_Energy_Share_(%)',
+        'Household_Energy_Use_(%)'
     ]
     df_clean['Country'] = df_clean['Country'].replace(replace_dict, regex=True)
     for col in columns_to_fill:
         df_clean[col] = df_clean.groupby("Country")[col].transform(lambda x: x.fillna(x.median()))
     for col in collumns_to_check:
         df_clean[col] = df_clean[col].clip(0, 100)
-    df_clean = df_clean[df_clean["Fossil Fuel Dependency (%)"].between(0, 100)]
+    df_clean = df_clean.groupby([ "Country","Year"])[columns_to_fill].mean().reset_index()
+    df_clean = df_clean[df_clean["Fossil_Fuel_Dependency_(%)"].between(0, 100)]
     df_clean = df_clean[df_clean["Year"] > 1999]
     df_clean = df_clean.round(4)
-    df_clean.to_csv("SRC/DATA/Silver.csv", index=False)
+    df_clean.to_csv("SRC/DATA/SILVER/Silver.csv", index=False)
+    df_clean.to_sql(name='Silver_Data', con=engine, if_exists='append')
+    unico = df_clean['Country'].unique()
+    for uni in unico:
+        df = df_clean.groupby("Country").get_group(uni).reset_index()
+        df_noindex = df.drop('index', axis=1)
+        df_noindex.to_csv(f'SRC/DATA/SILVER/{uni}_silver.csv', index=False)
+        df.to_sql(name=f'{uni}_silver', con=engine, if_exists='append')
 
-    parte = pd.read_csv('SRC/DATA/Silver.csv', iterator=True, chunksize=1000 )
-    while True:
-        try:
-            t_start = time()
-            df = next(parte)
-            df.to_sql(name='Silver_Data', con=engine, if_exists='append')
-            t_end = time()
-            print('uma parte levou %.3f segundos' % (t_end - t_start))
-        except StopIteration:
-            print("fim dos dados limpos")
-            break
+
+   
